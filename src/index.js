211ecfdd9345c5217cfe4a383e6ebdbf600c6999
Sazzad Hossain Khan <itsazzad@gmail.com>
2022-11-09 16:53:51 +0600

    webtrigger

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-export const run = () => {
-  console.log('Hello world!');
+const buildOutput = (rnd) => ({
+  body: '{"hello": "world"}',
+  headers: {
+    'Content-Type': ['application/json'],
+    'X-Request-Id': [`rnd-${rnd}`]
+  },
+  statusCode: 200,
+  statusText: 'OK'
+});
+
+exports.runAsync = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = buildOutput(Math.random());
+      resolve(result);
+    }, 1000);
+  });
+};
+
+exports.runSync = () => {
+  const result = buildOutput(Math.random());
+  return result;
 };
